f0ba3b5da774c2fd401e5ad812b787da91564155
juanmafdez <115906171+juanmafdez@users.noreply.github.com>
2024-09-13 02:27:07 -0500

    Update architecture.py
    optimization for get_resnet_backbone

--- swav/architecture.py
+++ swav/architecture.py
@@ -1,21 +1,20 @@
-from tensorflow.keras import layers
-from tensorflow.keras import models
+from tensorflow.keras import layers, models
 from tensorflow.keras.layers import Lambda
 import tensorflow as tf
 
 def get_resnet_backbone():
 	base_model = tf.keras.applications.ResNet50(include_top=False,
-		weights=None, input_shape=(None, None, 3))
+		weights=None, input_shape=(100, 100, 3))
 	base_model.trainable = True
 
-	inputs = layers.Input((None, None, 3))
+	inputs = layers.Input((100, 100, 3))
 	h = base_model(inputs, training=True)
 	h = layers.GlobalAveragePooling2D()(h)
 	backbone = models.Model(inputs, h)
 
 	return backbone
 
-def get_projection_prototype(dense_1=1024, dense_2=96, prototype_dimension=10):
+def get_projection_prototype(dense_1=1024, dense_2=256, prototype_dimension=100):
 	inputs = layers.Input((2048, ))
 	projection_1 = layers.Dense(dense_1)(inputs)
 	projection_1 = layers.BatchNormalization()(projection_1)
