936f8cef6f824c7c882efda8de0b442bbbe94090
juanmafdez <115906171+juanmafdez@users.noreply.github.com>
2026-02-20 19:24:19 -0500

    Update multicrop_dataset.py
    Fixed issues from dimensions in get_multires_dataset

--- swav/multicrop_dataset.py
+++ swav/multicrop_dataset.py
@@ -1,21 +1,17 @@
 import tensorflow as tf
 import random
 
-AUTO = tf.data.experimental.AUTOTUNE
-
-# Reference: https://github.com/google-research/simclr/blob/master/data_util.py
+AUTO = tf.data.AUTOTUNE
 
 @tf.function
 def gaussian_blur(image, kernel_size=23, padding='SAME'):
 	sigma = tf.random.uniform((1,))* 1.9 + 0.1
-
 	radius = tf.cast(kernel_size / 2, tf.int32)
 	kernel_size = radius * 2 + 1
+	
 	x = tf.cast(tf.range(-radius, radius + 1), tf.float32)
-	blur_filter = tf.exp(
-		-tf.pow(x, 2.0) / (2.0 * tf.pow(tf.cast(sigma, tf.float32), 2.0)))
+	blur_filter = tf.exp(-tf.pow(x, 2.0) / (2.0 * tf.pow(tf.cast(sigma, tf.float32), 2.0)))
 	blur_filter /= tf.reduce_sum(blur_filter)
-	# One vertical and one horizontal filter.
 	blur_v = tf.reshape(blur_filter, [kernel_size, 1, 1, 1])
 	blur_h = tf.reshape(blur_filter, [1, kernel_size, 1, 1])
 	num_channels = tf.shape(image)[-1]
@@ -24,10 +20,8 @@ def gaussian_blur(image, kernel_size=23, padding='SAME'):
 	expand_batch_dim = image.shape.ndims == 3
 	if expand_batch_dim:
 		image = tf.expand_dims(image, axis=0)
-	blurred = tf.nn.depthwise_conv2d(
-		image, blur_h, strides=[1, 1, 1, 1], padding=padding)
-	blurred = tf.nn.depthwise_conv2d(
-		blurred, blur_v, strides=[1, 1, 1, 1], padding=padding)
+	blurred = tf.nn.depthwise_conv2d(image, blur_h, strides=[1, 1, 1, 1], padding=padding)
+	blurred = tf.nn.depthwise_conv2d(blurred, blur_v, strides=[1, 1, 1, 1], padding=padding)
 	if expand_batch_dim:
 		blurred = tf.squeeze(blurred, axis=0)
 	return blurred
@@ -48,38 +42,6 @@ def color_drop(x):
 	return x
 
 @tf.function
-def custom_augment(image):
-	# Random flips
-	image = random_apply(tf.image.flip_left_right, image, p=0.5)
-	# Randomly apply gausian blur
-	image = random_apply(gaussian_blur, image, p=0.5)
-	# Randomly apply transformation (color distortions) with probability p.
-	image = random_apply(color_jitter, image, p=0.8)
-	# Randomly apply grayscale
-	image = random_apply(color_drop, image, p=0.2)
-
-	return image
-
-@tf.function
-def random_resize_crop(image, min_scale, max_scale, crop_size):
-	# Conditional resizing
-	if crop_size == 86:
-		image_shape = 100
-		image = tf.image.resize(image, (image_shape, image_shape))
-	else:
-		image_shape = 64
-		image = tf.image.resize(image, (image_shape, image_shape))
-	# Get the crop size for given min and max scale
-	size = tf.random.uniform(shape=(1,), minval=min_scale*image_shape,
-		maxval=max_scale*image_shape, dtype=tf.float32)
-	size = tf.cast(size, tf.int32)[0]
-	# Get the crop from the image
-	crop = tf.image.random_crop(image, (size,size,3))
-	crop_resize = tf.image.resize(crop, (crop_size, crop_size))
-
-	return crop_resize
-
-@tf.function
 def random_apply(func, x, p):
 	return tf.cond(
 		tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32),
@@ -88,22 +50,30 @@ def random_apply(func, x, p):
 		lambda: x)
 
 @tf.function
-def scale_image(image):
-	image = tf.image.convert_image_dtype(image, tf.float32)
+def custom_augment(image):
+	image = random_apply(tf.image.flip_left_right, image, p=0.5)
+	image = random_apply(gaussian_blur, image, p=0.5)
+	image = random_apply(color_jitter, image, p=0.8)
+	image = random_apply(color_drop, image, p=0.2)
 	return image
 
 @tf.function
-def tie_together(image, min_scale, max_scale, crop_size):
-	# Retrieve the image features
-	image = image
-	# Scale the pixel values
-	image = scale_image(image)
-	# Random resized crops
-	image = random_resize_crop(image, min_scale,
-		max_scale, crop_size)
-	# Color distortions & Gaussian blur
-	image = custom_augment(image)
+def random_resize_crop(image, min_scale, max_scale, crop_size):
+	h = tf.shape(image)[0]
+	w = tf.shape(image)[1]
+	base_dim = tf.cast(tf.minimum(h, w), tf.float32)
 
+	rand_size = tf.random.uniform([], min_scale*base_dim, max_scale*base_dim, dtype=tf.float32)
+	rand_size = tf.cast(tf.clip_by_value(rand_size, 2.0, base_dim), tf.int32)
+
+	crop = tf.image.random_crop(image, size=[rand_size, rand_size, 3])
+	crop_resize = tf.image.resize(crop, (crop_size, crop_size))
+	return crop_resize
+
+@tf.function
+def tie_together(image, min_scale, max_scale, crop_size):
+	image = random_resize_crop(image, min_scale,max_scale, crop_size)
+	image = custom_augment(image)
 	return image
 
 def get_multires_dataset(dataset,
@@ -113,23 +83,14 @@ def get_multires_dataset(dataset,
 	max_scale,
 	options=None):
 	loaders = tuple()
-	for i, num_crop in enumerate(num_crops):
-		for _ in range(num_crop):
-			loader = (
-					dataset
-					.shuffle(1024)
-					.map(lambda x: tie_together(x, min_scale[i],
-						max_scale[i], size_crops[i]), num_parallel_calls=AUTO)
+	base = dataset.with_options(options) if options is not None else dataset
+		
+	for i, n in enumerate(num_crops):
+		for _ in range(n):
+			loader = base.map(
+					lambda x: tie_together(x, min_scale[i], max_scale[i], size_crops[i]),
+					num_parallel_calls=AUTO,
+					deterministic=False
 				)
-			if options!=None:
-				loader = loader.with_options(options)
-			loaders += (loader, )
-
+			loaders += (loader,)
 	return loaders
-
-def shuffle_zipped_output(a,b,c,d,e):
-	listify = [a,b,c,d,e]
-	random.shuffle(listify)
-
-	return listify[0], listify[1], listify[2], \
-		listify[3], listify[4]
