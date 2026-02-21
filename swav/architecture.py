79f4e5171aa0678ff74d25b250b2b07d4d000f5f
juanmafdez <115906171+juanmafdez@users.noreply.github.com>
2026-02-20 19:32:31 -0500

    Update architecture.py
    Refactored projection_prototype architecture

--- swav/architecture.py
+++ swav/architecture.py
@@ -6,24 +6,18 @@ def get_resnet_backbone():
 	base_model = tf.keras.applications.ResNet50(include_top=False,
 		weights=None, input_shape=(None, None, 3))
 	base_model.trainable = True
-
 	inputs = layers.Input((None, None, 3))
 	h = base_model(inputs, training=True)
 	h = layers.GlobalAveragePooling2D()(h)
 	backbone = models.Model(inputs, h)
-
 	return backbone
 
 def get_projection_prototype(dense_1=1024, dense_2=256, prototype_dimension=100):
 	inputs = layers.Input((2048, ))
-	projection_1 = layers.Dense(dense_1)(inputs)
-	projection_1 = layers.BatchNormalization()(projection_1)
-	projection_1 = layers.Activation("relu")(projection_1)
-
-	projection_2 = layers.Dense(dense_2)(projection_1)
-	projection_2_normalize = Lambda(lambda x: tf.math.l2_normalize(x, axis=1), name='projection')(projection_2)
-
-	prototype = layers.Dense(prototype_dimension, use_bias=False, name='prototype')(projection_2_normalize)
-
-	return models.Model(inputs=inputs,
-		outputs=[projection_2_normalize, prototype])
+	x = layers.Dense(dense_1)(inputs)
+	x = layers.BatchNormalization()(x)
+	x = layers.Activation("relu")(x)
+	x = layers.Dense(dense_2)(x)
+	proj = Lambda(lambda z: tf.math.l2_normalize(z, axis=1), name='projection')(x)
+	prototype = layers.Dense(prototype_dimension, use_bias=False, name='prototype')(proj)
+	return models.Model(inputs=inputs, outputs=[proj, prototype])
